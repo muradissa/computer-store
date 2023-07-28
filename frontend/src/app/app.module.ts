7bd94f83a9663c546f17cdd79af54db7648445f1
Murad EIssa <muradissa00@gmail.com>
2023-07-28 16:15:19 +0300

    delete stars from the card in home page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { RatingModule } from 'ng-starrating';
+// import { RatingComponent } from 'ng-starrating/lib/rating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // RatingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
